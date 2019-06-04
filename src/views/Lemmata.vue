10687051b3a18436b5eae4eae51aebeeb4cb41e1
Lukas Rosenthaler <rosenth@iml-foxi.iml.unibas.ch>
2019-06-04 16:43:41 +0200

    table component from vuetify

--- src/views/Lemmata.vue
+++ src/views/Lemmata.vue
@@ -1,100 +1,123 @@
-<template> 
-<div>
-    <alphabetindex v-on:char_selected="startchar_changed"
-                   v-bind:startchar="startchar"
-                   v-bind:getData="getStartPage">
-    </alphabetindex>
-    <div>{{ nitems }} Einträge gefunden</div>
-    <table>
-        <tr><th>Lemma</th><th>Von</th><th>Bis</th></tr>
-        <tr v-on:click="gotoLemma(lemma.iri)"
-            v-for="lemma in lemmata"
-            v-bind:key="lemma.iri">
-            <td>{{ lemma.props['mls:hasLemmaText'][0].strval }}</td>
-            <td>{{ lemma.props.hasOwnProperty('mls:hasStartDate') ? lemma.props['mls:hasStartDate'][0].strval : '?' }}</td>
-            <td>{{ lemma.props.hasOwnProperty('mls:hasEndDate') ? lemma.props['mls:hasEndDate'][0].strval : '?' }}</td>
-        </tr>
-    </table>
-    <v-pagination
-            v-model="page"
-            :length="npages"
-            value="page"
-            v-on:input="page_changed_to">
-    </v-pagination>
+<template>
+    <div>
+        <alphabetindex v-on:char_selected="startchar_changed"
+                       v-bind:startchar="startchar"
+                       v-bind:getData="getStartPage">
+        </alphabetindex>
+        <div>{{ nitems }} Einträge gefunden</div>
+        <!--
+        <table>
+            <tr><th>Lemma</th><th>Von</th><th>Bis</th></tr>
+            <tr v-on:click="gotoLemma(lemma.iri)"
+                v-for="lemma in lemmata"
+                v-bind:key="lemma.iri">
+                <td>{{ lemma.props['mls:hasLemmaText'][0].strval }}</td>
+                <td>{{ lemma.props.hasOwnProperty('mls:hasStartDate') ? lemma.props['mls:hasStartDate'][0].strval : '?' }}</td>
+                <td>{{ lemma.props.hasOwnProperty('mls:hasEndDate') ? lemma.props['mls:hasEndDate'][0].strval : '?' }}</td>
+            </tr>
+        </table>
+        -->
+        <v-data-table
+                :headers="headers"
+                :items="lemmata"
+                :loading="isloading"
+                class="elevation-1"
+                hide-actions
+        >
+            <v-progress-linear v-slot:progress color="blue" indeterminate></v-progress-linear>
+            <template v-slot:items="props">
+                <td>{{ props.item.props['mls:hasLemmaText'][0].strval }}</td>
+                <td>{{ props.item.props.hasOwnProperty('mls:hasStartDate') ? props.item.props['mls:hasStartDate'][0].strval : '?'}}</td>
+                <td>{{ props.item.props.hasOwnProperty('mls:hasEndDate') ? props.item.props['mls:hasEndDate'][0].strval : '?'}}</td>
+            </template>
+        </v-data-table>
+        <v-pagination
+                v-model="page"
+                :length="npages"
+                value="page"
+                v-on:input="page_changed_to">
+        </v-pagination>
 
-</div>
+    </div>
 </template>
 
 <script>
-import axios from 'axios';
-import router from '../router';
-import {simplify_data} from '../lib/jsonld_simplifier';
-import {lemmata_query} from '../lib/queries';
-import alphabetindex from '../components/AlphabetindexComponent';
+  import axios from 'axios';
+  import router from '../router';
+  import {simplify_data} from '../lib/jsonld_simplifier';
+  import {lemmata_query} from '../lib/queries';
+  import alphabetindex from '../components/AlphabetindexComponent';
 
-export default {
+  export default {
     name: 'lemmata',
     data: function() {
-       return {
-           startchar: 'A',
-           page: 1,
-           nitems: 0,
-           pagesize: 25,
-           lemmata: [],
-        } 
+      return {
+        startchar: 'A',
+        page: 1,
+        nitems: 0,
+        pagesize: 25,
+        lemmata: [],
+        isloading: false,
+        headers: [
+          {name: "Lemma", value: "props['mls:hasLemmaText'][0].strval"},
+          {name: "Von", value: "props['mls:hasStartDate'][0].strval"},
+          {name: "Bis", value: "props['mls:hasEndDate'][0].strval"}
+        ]
+      }
     },
     computed: {
-        npages: function() {
-            return Math.ceil(this.nitems / this.pagesize);
-        }
+      npages: function() {
+        return Math.ceil(this.nitems / this.pagesize);
+      }
     },
 
     components: {
-        alphabetindex
+      alphabetindex
     },
     methods: {
-        page_changed_to: function(pagenum) {
-           this.page = pagenum;
-           this.getPage(this.startchar, this.page - 1);
+      page_changed_to: function(pagenum) {
+        this.page = pagenum;
+        this.getPage(this.startchar, this.page - 1);
+      },
+      startchar_changed: function(val) {
+        this.startchar = val;
+      },
+      getPage: function(ch, page)  {
+        axios({
+          method: 'post',
+          url: 'https://api.dasch.swiss/v2/searchextended',
+          header: {'Content-Type': 'text/plain; charset=utf-8'},
+          data: lemmata_query({page: page, start: ch})
+        }).then(
+          response => (this.lemmata = simplify_data(response.data))
+        ).catch(function (error) {
+          console.log(error);
+        })
+      },
+      getStartPage: function(ch, page = 0) {
+        this.startchar = ch;
+        this.page = page + 1;
+        this.isloading = true;
+        axios({
+          method: 'post',
+          url: 'https://api.dasch.swiss/v2/searchextended/count',
+          header: {'Content-Type': 'text/plain; charset=utf-8'},
+          data: lemmata_query({page: 0, start: ch})
+        }).then(
+          response => (this.nitems = response.data['schema:numberOfItems'])
+        ).catch(function (error) {
+          console.log(error)
+        });
+        this.getPage(ch, page);
+        this.isloading = false;
+      },
 
-        },
-        startchar_changed: function(val) {
-            this.startchar = val;
-        },
-        getPage: function(ch, page)  {
-            axios({
-                method: 'post',
-                url: 'https://api.dasch.swiss/v2/searchextended',
-                header: {'Content-Type': 'text/plain; charset=utf-8'},
-                data: lemmata_query({page: page, start: ch})
-            }).then(
-                response => (this.lemmata = simplify_data(response.data))
-            ).catch(function (error) {
-                console.log(error);
-            })
-        },
-        getStartPage: function(ch, page = 0) {
-            this.startchar = ch;
-            this.page = page + 1;
-            axios({
-                method: 'post',
-                url: 'https://api.dasch.swiss/v2/searchextended/count',
-                header: {'Content-Type': 'text/plain; charset=utf-8'},
-                data: lemmata_query({page: 0, start: ch})
-            }).then(
-                response => (this.nitems = response.data['schema:numberOfItems'])
-            ).catch(function (error) {
-                console.log(error)
-            });
-            this.getPage(ch, page);
-        },
-
-        gotoLemma: function(iri) {
-            router.push({ path: '/lemma/' + encodeURIComponent(iri)})
-        }
+      gotoLemma: function(iri) {
+        router.push({ path: '/lemma/' + encodeURIComponent(iri)})
+      }
     },
     mounted () {
-        this.getStartPage(this.startchar, this.page - 1);
+      this.getStartPage(this.startchar, this.page - 1);
     }
-}
+  }
 </script>
