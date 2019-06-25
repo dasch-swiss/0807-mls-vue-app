2f3082fa5e6c152781decc6827ea4d52371ebf3a
Lukas Rosenthaler <rosenth@iml-foxi.iml.unibas.ch>
2019-06-25 18:03:08 +0200

    Start Page

--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -1,5 +1,24 @@
 <template>
+  <v-container grid-list-xl>
+    <v-layout justify-center>
+      Die Information und der Zugang zu bereits publizierten oder Online gestellten Lexika
+    </v-layout>
+    <div>
 
+    </div>
+    <v-layout align-center justify-space-between fill-height column>
+      <v-flex mb-4>
+        <v-btn color="info" to="/lemmata">
+          Lemmata
+        </v-btn>
+      </v-flex>
+      <v-flex mb-4>
+        <v-btn color="info" to="/lexicons">
+          Lexika
+        </v-btn>
+      </v-flex mb-4>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
